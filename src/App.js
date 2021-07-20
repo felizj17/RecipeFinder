2710133d487675303fb92e1cefeb0ad5edf01f38
felizj17 <felizj17@gmail.com>
2021-07-20 19:19:36 -0400

    made responsive using bootstrap

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import React, { Component,useEffect, useState } from 'react';
+import React, {useEffect, useState } from 'react';
 import Recipe from "./Recipe";
 import './App.css';
 
 const App=()=>{
+
+
   const APP_ID="98fb884e";
   const APP_KEY="e7d2c2d8c0a5a798d0b55a8a4e6a671c" 
   const exampleRequest=`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
@@ -26,18 +28,35 @@ const App=()=>{
     setSearch('')
   }
   return(
+    
     <div className="App">
-      <form onSubmit ={getSearch} className ="searchForm">
-        <input className="searchBar" type ="text" value={search} onChange={updateSearch}/>
-        <button onClick = {getSearch} className="searchButton" type="submit">Search</button>
-      </form>
-      {recipes.map(recipe =>(
+
+      <div className="container">
+        <header className="title">
+          <h1>Recipe Finder</h1>
+        </header>
+        
+        <form onSubmit ={getSearch} className ="searchForm mb-3 w-100">
+          <input className="searchBar w-75" type ="text" value={search} onChange={updateSearch}/>
+          <button onClick = {getSearch} className="searchButton" type="submit">Search</button>
+        </form>
+      </div>
+      <div className="container border-start border-end">
+     
+      <div className="d-flex flex-wrap justify-content-center ">
+      {recipes.map((recipe) =>(
         <Recipe 
         key={recipe.recipe.key}
-        title={recipe.recipe.label} 
+        title={recipe.recipe.label}
+        ingredients={recipe.recipe.ingredients}
         calories={recipe.recipe.calories} 
-        image={recipe.recipe.image}/>
+        image={recipe.recipe.image}
+        url={recipe.recipe.url}
+        source={recipe.recipe.source}
+        />
       ))}
+      </div>
+    </div>
     </div>
   )
 }
--- src/Recipe.js
+++ src/Recipe.js
@@ -1,14 +1,47 @@
-import React from'react';
+import { useState } from "react";
 
-
-const Recipe = (props)=>{
-    
-    return(
-        <div>
-            <h1>{props.title}</h1>
-            <p>{props.calories}</p>
-            <img src={props.image}/>
+const Recipe = (props) => {
+  const [click, setClick] = useState(false);
+  console.log(props.ingredients)
+  return (
+    <div className={click?"m-auto card-group text-decoration-none":"m-2 card-group text-decoration-none"} style={click?{width: '75rem', display:"block" }:{width:'25rem'}}>
+      <div
+        onClick={()=>{setClick(!click)}}
+        className="card crd border border-2 border-secondary rounded m-3"
+      >
+        <div className="w-50 crd-img text-center mx-auto d-block">
+          <img
+            className={
+              click
+                ? "img-fluid mt-5 fluid-center w-100 border border-warning border-3 rounded"
+                : "img-fluid mt-5 fluid-center w-100 border border-warning border-3 rounded-circle"
+            }
+            src={props.image}
+          />
         </div>
-    );
+        <h2 className="w-75 mx-auto">{props.title}</h2>
+        <p className="pb-5">
+          Total Calories:
+          <span className="ms-2 ">{Math.round(props.calories)}</span>
+        </p>
+        {click&&
+        <div className="text-start ms-1 pb-5">
+          <h3 className="ms-1 mb-3" >Ingredients</h3>
+            {props.ingredients.map(ingredients=>(
+              <div className="w-75 ms-2 mb-2">
+                <p className="ms-3">-{ingredients.text}</p>
+              </div>
+            ))
+            }
+        </div>}
+        <a 
+          className="me-3  link align-bottom "
+          href={props.url}
+          target="_blank" 
+          data-bs-toggle="tooltip"
+          title={`Visit ${props.source} to view the recipe`}>View Recipe </a>
+      </div>
+    </div>
+  );
 };
 export default Recipe;
